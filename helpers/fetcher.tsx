480f9e854fb18b1b1fd74835aef850571411060b
Matthew Smith <musicsmith4@gmail.com>
2021-04-06 13:58:46 -0400

    update fetcher

--- helpers/fetcher.tsx
+++ helpers/fetcher.tsx
@@ -1,5 +1,14 @@
 const fetcher = (
   ...args: [input: RequestInfo, init?: RequestInit | undefined]
-) => fetch(...args).then((response) => response.json());
+) =>
+  fetch(...args)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('An error occurred.');
+      }
+    })
+    .catch((error) => error);
 
 export default fetcher;
