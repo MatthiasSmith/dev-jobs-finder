1802392f3e67a676856afbc1d0e73f7608f948e5
Matthew Smith <musicsmith4@gmail.com>
2021-04-05 21:22:20 -0400

    update layout and styles for job items and app

--- components/job-item.tsx
+++ components/job-item.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import utilStyles from '../styles/utils.module.css';
+import DateString from './date';
 import styles from './job-item.module.css';
 
 const JobItem = ({
@@ -12,34 +14,54 @@ const JobItem = ({
   location,
   created_at,
 }: {
-  id: string,
-  company_logo: string,
-  title: string,
-  company: string,
-  type: string,
-  location: string,
-  created_at: string
+  id: string;
+  company_logo: string;
+  title: string;
+  company: string;
+  type: string;
+  location: string;
+  created_at: string;
 }) => {
   return (
-    <li className={`${utilStyles.card} ${utilStyles.noListStyle}`}>
-      <h3 className={utilStyles.headingMd}>
-        {company_logo ? (
-          <Image
-            src={company_logo}
-            width={80}
-            height={80}
-            layout='fixed'
-            objectFit='contain'
-          ></Image>
-        ) : null}
-        {title}
-      </h3>
-      <div>
-        <h4 className={utilStyles.headingSm}>{company}</h4>
-      </div>
-      <p>job-type: {type}</p>
-      <p>location: {location}</p>
-      <p>posted on: {new Date(created_at).toDateString()}</p>
+    <li
+      className={`${utilStyles.card} ${utilStyles.noListStyle} ${styles.jobItem}`}
+    >
+      <Link href={`jobs/${id}`}>
+        <a>
+          <div
+            className={`${utilStyles.flexRow} ${utilStyles.alignCenter} ${utilStyles.mb1}`}
+          >
+            <div className={`${styles.logoContainer}`}>
+              {company_logo ? (
+                <Image
+                  src={company_logo}
+                  width={65}
+                  height={65}
+                  layout='fixed'
+                  objectFit='contain'
+                  className={styles.companyLogo}
+                ></Image>
+              ) : null}
+            </div>
+            <div>
+              <span className={utilStyles.textColorSecondary}>{company}</span>
+              <h3 className={utilStyles.headingSm}>{title}</h3>
+            </div>
+          </div>
+          <div
+            className={`${utilStyles.flexRow} ${utilStyles.alignCenter} ${utilStyles.spaceBetween} ${utilStyles.textColorSecondary}`}
+          >
+            <div>
+              <span>{type}</span> - <span>{location}</span>
+            </div>
+            <div
+              className={`${utilStyles.textColorSecondary}`}
+            >
+              <DateString dateString={created_at} />
+            </div>
+          </div>
+        </a>
+      </Link>
     </li>
   );
 };
