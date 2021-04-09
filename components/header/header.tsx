dd0441058f6fcce8586792d24378ebcda86acdd9
Matthew Smith <musicsmith4@gmail.com>
2021-04-09 13:59:26 -0400

    make logo a link

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
+
 import styles from './header.module.css';
 
 const Header = ({ children }: { children: React.ReactNode }) => {
   return (
     <header className={styles.header}>
-      <span className={styles.logo}>devjobfinder</span>
+      <Link href='/'>
+        <a className={styles.logo}>devjobsfinder</a>
+      </Link>
       <div className={styles.childrenContainer}>{children}</div>
     </header>
   );
