aa149e0064d7e6d226ee41000297146373a3e548
Matthew Smith <musicsmith4@gmail.com>
2021-04-14 21:32:37 -0400

    fix month formatting

--- components/date/date.tsx
+++ components/date/date.tsx
@@ -3,12 +3,17 @@ import { formatDistanceToNowStrict } from 'date-fns';
 const DateString = ({ dateString }: { dateString: string }) => {
   const formatted = formatDistanceToNowStrict(new Date(dateString));
   let shortened = formatted.replace(/years?/g, 'y');
-  shortened = shortened.replace(/months?/g, 'd');
+  shortened = shortened.replace(/month?/g, ' mth');
+  shortened = shortened.replace(/months?/g, ' mths');
   shortened = shortened.replace(/days?/g, 'd');
   shortened = shortened.replace(/hours?/g, 'h');
   shortened = shortened.replace(/minute?/g, ' min');
   shortened = shortened.replace(/minutes?/g, ' mins');
-  return <span style={{whiteSpace: 'nowrap'}}>{shortened.replace(' ', '')} ago</span>;
+  return (
+    <span style={{ whiteSpace: 'nowrap' }}>
+      {shortened.replace(' ', '')} ago
+    </span>
+  );
 };
 
 export default DateString;
